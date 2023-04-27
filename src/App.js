809bebb4c80d2819cc4dce7d08c43ab530365a98
AlexJackson01 <ajjackson22@gmail.com>
2023-04-27 15:11:41 +0100

    Set up GameEngine and rendered bird on screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Constants from './Constants'
+import { GameEngine } from "react-native-game-engine";
+import Matter from 'matter-js'
+import { useRef, useState } from "react";
+import Bird from './Bird'
 
         
 const App = () => {
+
+    const gameEngine = useRef(null)
+
+    const setupWorld = () => {
+        let engine = Matter.Engine.create({ enableSleeping: false })
+        let world = engine.world;
+
+        let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50)
+
+        Matter.World.add(world, [bird])
+
+        return {
+            physics: { engine: engine, world: world },
+            bird: { body: bird, size: [50, 50], color: 'red', renderer: Bird }
+        }
+    }
+
     return (
         <View>
-             
+             <GameEngine ref={gameEngine} style={styles.gameContainer} entities={setupWorld()}/>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+
+})
 export default App;
